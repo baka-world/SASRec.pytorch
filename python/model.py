6e0e7a929419717724cb1d02c078522279e270eb
Yimo Lieu <yimolieu@163.com>
2026-01-08 13:27:17 +0800

    fix: use -1e4 for FP16 compatibility

--- python/model.py
+++ python/model.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 import sys
 
-FLOAT_MIN = -1e9
+FLOAT_MIN = -1e4
 
 
 class NumericallyStableMultiheadAttention(torch.nn.Module):
--- python/model_mhc.py
+++ python/model_mhc.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 import sys
 
-FLOAT_MIN = -1e9
+FLOAT_MIN = -1e4
 
 
 def sinkhorn_knopp(M, max_iter=20):
